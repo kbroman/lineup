50c5e607ed5690fb9a915f18702812ed52fd657c
Karl Broman <kbroman@gmail.com>
2011-03-17 09:23:41 -0500

    Add function pulldiag()

---
+++ R/pulldiag.R
@@ -0,0 +1,47 @@
+######################################################################
+#
+# pulldiag.R
+#
+# copyright (c) 2011, Karl W Broman
+# last modified Mar, 2011
+# first written Mar, 2011
+#
+#     This program is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU General Public License,
+#     version 3, as published by the Free Software Foundation.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but without any warranty; without even the implied warranty of
+#     merchantability or fitness for a particular purpose.  See the GNU
+#     General Public License, version 3, for more details.
+# 
+#     A copy of the GNU General Public License, version 3, is available
+#     at http://www.r-project.org/Licenses/GPL-3
+# 
+# Part of the R/lineup package
+# Contains: pulldiag
+#
+######################################################################
+
+##############################
+# pull out the "diagonal" from a distance matrix
+#     (the self-self cases)
+##############################
+
+pulldiag <-
+function(d)
+{
+  rn <- rownames(d)
+  cn <- colnames(d)
+  m <- match(rn, cn)
+  self <- rep(NA, sum(!is.na(m)))
+  wh <- which(!is.na(m))
+  names(self) <- rn[wh]
+  m <- m[!is.na(m)]
+  for(i in seq(along=wh)) 
+    self[i] <- d[wh[i],m[i]]
+
+  self
+}
+  
+# end of pulldiag.R
