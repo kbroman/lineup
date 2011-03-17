54d559655d3fe6b0b7e1c212ed516347776545c2
Karl Broman <kbroman@gmail.com>
2011-03-17 10:36:36 -0500

    Add function lineupversion()

---
+++ R/util.R
@@ -0,0 +1,36 @@
+######################################################################
+#
+# util.R
+#
+# copyright (c) 2011, Karl W Broman
+# last modified Mar, 2011
+# first written Mar, 2011
+#
+#     This program is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU General Public License,
+#     version 3, as published by the Free Software Foundation.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but without any warranty; without even the implied warranty of
+#     merchantability or fitness for a particular purpose.  See the GNU
+#     General Public License, version 3, for more details.
+# 
+#     A copy of the GNU General Public License, version 3, is available
+#     at http://www.r-project.org/Licenses/GPL-3
+# 
+# Part of the R/lineup package
+# Contains: lineupversion
+#
+######################################################################
+
+######################################################################
+# print the installed version of R/lineup
+######################################################################
+lineupversion <-
+function()
+{
+  u <- strsplit(library(help=lineup)[[3]][[1]][2]," ")[[1]]
+  u[length(u)]
+}
+
+# end of util.R
