841b256caa506ace311a006105c788eed2fbbeb3
Karl Broman <kbroman@gmail.com>
2011-04-07 14:28:09 -0500

    Add 'what' argument to plot.lineupdist.

--- R/plot.lineupdist.R
+++ R/plot.lineupdist.R
@@ -24,29 +24,43 @@
 ######################################################################
 
 plot.lineupdist <-
-function(x, breaks, add.rug=TRUE, ...)
+function(x, breaks, add.rug=TRUE, what=c("both", "ss", "sn"), ...)
 {
+  if(what=="ns") what <- "sn"
+  what <- match.arg(what)
+
   di <- pulldiag(x)
-  ra <- range(x, na.rm=TRUE)
+  if(what=="both") ra <- range(x, na.rm=TRUE)
+  else if(what=="ss") ra <- range(di, na.rm=TRUE)
+  else ra <- range(omitdiag(x), na.rm=TRUE)
+
+  if(diff(ra)==0) ra <- ra+c(-0.001, 0.001)
+
   if(missing(breaks)) breaks <- seq(ra[1], ra[2], len=sqrt(prod(dim(x))))
   if(length(breaks)==1) breaks <- seq(ra[1], ra[2], len=breaks)
   d.method <- switch(attr(x, "d.method"), "cor"="correlation", "rmsd"="RMS distance")
   main <- paste(c("Self-self", "Self-nonself"), switch(attr(x, "d.method"), "cor"="correlation", "rmsd"="distance"))
 
-  if(!all(is.na(di))) { # some self-self distances
-    
-    old.mfrow <- par("mfrow")
-    old.las <- par("las")
-    on.exit(par(mfrow=old.mfrow, las=old.las))
-    par(mfrow=c(2,1), las=1)
+  if(what != "sn" && !all(is.na(di))) { # some self-self distances
+
+    if(what == "both") {
+      old.mfrow <- par("mfrow")
+      old.las <- par("las")
+      on.exit(par(mfrow=old.mfrow, las=old.las))
+      par(mfrow=c(2,1), las=1)
+    }
 
     hist(di, breaks=breaks, xlab=d.method, main=main[1])
     if(add.rug) rug(di)
   }
   
-  x <- omitdiag(x)
-  hist(x, breaks=breaks, xlab=d.method, main=main[2])
-  if(add.rug) rug(x)
+  if(what != "ss") {
+    x <- omitdiag(x)
+    hist(x, breaks=breaks, xlab=d.method, main=main[2])
+    if(add.rug) rug(x)
+  }
+
+  invisible()
 }
 
 # end of plot.lineupdist.R
