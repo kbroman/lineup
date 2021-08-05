a7b938942e758d789ee7c343b975ef10169700dc
Karl Broman <kbroman@gmail.com>
2021-08-05 15:53:07 -0500

    Fix lineupversion() to handle a case like "0.50"

--- R/util.R
+++ R/util.R
@@ -20,6 +20,10 @@ lineupversion <-
 {
     version <- unlist(utils::packageVersion("lineup"))
 
-    # make it like #.#-#
-    paste(c(version,".","-")[c(1,4,2,5,3)], collapse="")
+    if(length(version) == 3) {
+        # make it like #.#-#
+        return( paste(c(version, ".", "-")[c(1,4,2,5,3)], collapse="") )
+    }
+
+    paste(version, collapse=".")
 }
